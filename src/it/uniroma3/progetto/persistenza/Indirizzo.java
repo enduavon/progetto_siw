1d0114629c023938b5fa88efaf8f4ca3e8d7ebad
Andrea <root@enduavon-Lenovo-G50-70>
2015-05-07 17:39:19 +0200

    refactor vari

--- src/it/uniroma3/progetto/persistenza/Indirizzo.java
+++ src/it/uniroma3/progetto/persistenza/Indirizzo.java
@@ -1,7 +1,5 @@
 package it.uniroma3.progetto.persistenza;
 
-
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -15,29 +13,31 @@ public class Indirizzo {
 
 	}
 
-	public Indirizzo(String via, String citt‡, String stato, String codiceZip, String paese) {
-		this.strada = via;
-		this.citt‡ = citt‡;
+	public Indirizzo(String via, String citt√†, String nazione, String codicePostale, 
+			String stato) {
+		this.via = via;
+		this.citt√† = citt√†;
+		this.nazione = nazione;
+		this.codicePostale = codicePostale;
 		this.stato = stato;
-		this.codiceZip = codiceZip;
-		this.paese = paese;
 	}
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
 
 	@Column(nullable = false)
-	private String strada;
+	private String via;
 
 	@Column(nullable = false)
-	private String citt‡;
+	private String citt√†;
+	private String nazione;
+
+	@Column(nullable = false)
+	private String codicePostale;
+
+	//pu√≤ essere nullo, perch√® non √® detto che una nazione abbia degli stati come gli USA
+	@Column(nullable = true)
 	private String stato;
-
-	@Column(nullable = false)
-	private String codiceZip;
-
-	@Column(nullable = false)
-	private String paese;
 
 
 	public long getId() {
@@ -47,28 +47,28 @@ public class Indirizzo {
 		this.id = id;
 	}
 	public String getStreet() {
-		return strada;
+		return via;
 	}
 	public void setStreet(String street) {
-		this.strada = street;
+		this.via = street;
 	}
 	public String getCity() {
-		return citt‡;
+		return citt√†;
 	}
 	public void setCity(String city) {
-		this.citt‡ = city;
+		this.citt√† = city;
 	}
 	public String getZipCode() {
-		return codiceZip;
+		return codicePostale;
 	}
 	public void setZipCode(String zipCode) {
-		this.codiceZip = zipCode;
+		this.codicePostale = zipCode;
 	}
 	public String getCountry() {
-		return paese;
+		return nazione;
 	}
 	public void setCountry(String country) {
-		this.paese = country;
+		this.nazione = country;
 	}
 	public String getState() {
 		return stato;
@@ -76,10 +76,12 @@ public class Indirizzo {
 	public void setState(String state) {
 		this.stato = state;
 	}
+	
 	@Override
 	public int hashCode() {
-		return this.codiceZip.hashCode();
+		return this.codicePostale.hashCode();
 	}
+	
 	@Override
 	public boolean equals(Object obj) {
 		Indirizzo ind =(Indirizzo)obj;
