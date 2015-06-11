892c3c33dc5150349b4bd9853c1624886e2c75fc
Andrea <root@enduavon-Lenovo-G50-70>
2015-06-11 02:23:04 +0200

    sistemate validation delle password + fix messaggio pattern data

---
+++ src/it/uniroma3/cikmed/beanController/ConfirmPasswordValidator.java
@@ -0,0 +1,36 @@
+package it.uniroma3.cikmed.beanController;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIInput;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+@FacesValidator("confirmPasswordValidator")
+public class ConfirmPasswordValidator implements Validator {
+	
+	@Override
+	public void validate(FacesContext context, UIComponent component,
+		Object value) throws ValidatorException {
+		
+	  String confermaPassword = value.toString();
+ 
+	  UIInput uiInputPassword = (UIInput) component.getAttributes().get("password");
+	  String password = uiInputPassword.getSubmittedValue().toString(); 
+
+	  //Qua ci pensa il required=true della pagina xhtml
+	  if (password == null || password.isEmpty() || confermaPassword == null
+		|| confermaPassword.isEmpty()) {
+			return;
+	  }
+ 
+	  //Infine si fa il confronto fra le due password inserite
+	  if (!password.equals(confermaPassword)) {
+		uiInputPassword.setValid(false);
+		throw new ValidatorException(new FacesMessage(
+			"Le due password devono coincidere."));
+	  }
+	}
+}
--- src/it/uniroma3/cikmed/beanController/PasswordValidator.java
+++ src/it/uniroma3/cikmed/beanController/PasswordValidator.java
@@ -5,7 +5,6 @@ import java.util.regex.Pattern;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
-import javax.faces.component.UIInput;
 import javax.faces.context.FacesContext;
 import javax.faces.validator.FacesValidator;
 import javax.faces.validator.Validator;
@@ -18,48 +17,32 @@ import javax.faces.validator.ValidatorException;
 @FacesValidator("passwordValidator")
 public class PasswordValidator implements Validator {
 	
-//	private static final String PASSWORD_PATTERN = "(/^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/)";
-// 	
-//	private Pattern pattern;
-//	private Matcher matcher;
-//	
-//	public PasswordValidator(){
-//		  pattern = Pattern.compile(PASSWORD_PATTERN);
-//	}
-// 
+	private static final String PASSWORD_PATTERN = "(/^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/)";
+ 	
+	private Pattern pattern;
+	private Matcher matcher;
+	
+	public PasswordValidator(){
+		  pattern = Pattern.compile(PASSWORD_PATTERN);
+	}
+ 
  
 	@Override
 	public void validate(FacesContext context, UIComponent component,
 		Object value) throws ValidatorException {
 		
-//	  matcher = pattern.matcher(value.toString());	
-	  String confermaPassword = value.toString();
- 
-	  UIInput uiInputPassword = (UIInput) component.getAttributes().get("password");
-	  String password = uiInputPassword.getValue().toString();  //l'errore precedente stava nel metodo submittedValue?
+	  matcher = pattern.matcher(value.toString());	
 
-	  //Qua ci pensa il required=true della pagina xhtml
-	  if (password == null || password.isEmpty() || confermaPassword == null
-		|| confermaPassword.isEmpty()) {
-			return;
-	  }
- 
-	  //Infine si fa il confronto fra le due password inserite
-	  if (!password.equals(confermaPassword)) {
-		uiInputPassword.setValid(false);
-		throw new ValidatorException(new FacesMessage(
-			"Le due password devono coincidere."));
-	  }
 	  
 	  //si vede se la password inserita matcha il pattern impostato 
-//	  if(!matcher.matches()){
-//		  
-//			FacesMessage msg = 
-//				new FacesMessage("Password non valida.");
-//			msg.setSeverity(FacesMessage.SEVERITY_ERROR);
-//			throw new ValidatorException(msg);
-//
-//		}
+	  if(!matcher.matches()){
+		  
+			FacesMessage msg = 
+				new FacesMessage("Password non valida.");
+			msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+			throw new ValidatorException(msg);
+
+		}
 
 	}
 }
